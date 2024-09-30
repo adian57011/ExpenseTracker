cddab149f2a5df49b126ea4facf6e11f2668f87a
adian57011 <adian57011@gmail.com>
2024-09-30 13:32:18 +0600

    Session Filter Added

---
+++ ExpenseTracker/Filters/SessionFilter.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+public class SessionFilter : IAuthorizationFilter
+{
+    public void OnAuthorization(AuthorizationFilterContext context)
+    {
+        var sessionId = context.HttpContext.Session.GetString("UserId");
+
+        if (string.IsNullOrEmpty(sessionId))
+        {
+            context.Result = new RedirectToActionResult("Index", "Auth", null);
+        }
+    }
+}
